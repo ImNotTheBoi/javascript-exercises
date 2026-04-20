a70bae34ba44038a360f7f54e9af1e6d18063312
ImNotTheBoi <174925899+ImNotTheBoi@users.noreply.github.com>
2026-04-21 01:54:39 +0800

    permutations

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,23 @@
-const permutations = function() {
-  
+const permutations = function(arr, permutationArr = []) {
+  arr = arr.flat(1)
+  if (arr.length === 0 || arr.length === 1) {
+    permutationArr.push(arr)
+  }
+  else { arr.forEach((num, index) => {
+    const remainingNumbers = arr.slice(0, index).concat(arr.slice(index + 1))
+    permutations(remainingNumbers).forEach(permutation => {
+          permutationArr.push([num].concat(permutation))
+    });
+  })};
+  return permutationArr
 };
-  
+
+  //if no value or one length array
+    //permutation = arr
+  //for each number in array
+    //permutations += an array with the first number + to each 
+      //permutations(the rest of the numbers in array) given in this function
+  //return permutations
+
 // Do not edit below this line
 module.exports = permutations;
--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,17 @@
-const pascal = function() {
-  
+const pascal = function(n, arr = [1]) {
+  //if one row or arr.length = n
+    //return arr
+  //for each num in array
+    //rowArr varaible
+    //divide array length
+        //if below that num, add it to right num
+        //if equal none
+        //if above, add it to left num
+    //if first or last number is not 1
+        //add 1 and the first/last position
+  //return pascal(n, rowArr)       
 };
   
+
 // Do not edit below this line
 module.exports = pascal;
